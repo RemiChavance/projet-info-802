fd41d0f4bc867903ed9303c1d973086a706b7894
RemiChavance <remi.chavance@orange.fr>
2023-02-23 13:00:15 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { SoapCalculService } from './services/soap-calcul.service';
 
 @Component({
   selector: 'app-root',
